4bb7eb785957e5d8882942dda975c6f5e588d869
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-27 08:42:24 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  result = []
+  array.each do |element|
+    result.push(element**2)
+  end
+  result
+  
+  #array.collect do |element| element * element end
 end
